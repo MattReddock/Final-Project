513c84e33d4c110832d7790d235be9bd2d74f9cd
owen_mkn <s191483@students.aie.edu.au>
2020-08-05 18:56:56 +0930

    Basic player, look, jump & walk

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //test
-    }
+    public CharacterController controller;
 
-    // Update is called once per frame
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+    Vector3 move;
+
     void Update()
     {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
         
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        move.Normalize();
+        
+        if (Input.anyKey)
+        {
+            controller.Move(move * speed * Time.deltaTime);
+        }
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        
+        controller.Move(velocity * Time.deltaTime);
     }
 }
+
