69c90e8d11dfe79f18ff2dd6179f87aef8630ce7
Mardukus01010011 <s191479@students.aie.edu.au>
2020-08-10 20:28:24 +0930

    small script changes to chicken movement

---
+++ Assets/Scripts/ChickenMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenMovement : MonoBehaviour
+{
+	private Animator anim;
+    private UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    public float runAwayDistance = 8.0f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < runAwayDistance)
+        {
+            Vector3 dirToPlayer = transform.position - player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+
+            agent.SetDestination(newPos);
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+}
