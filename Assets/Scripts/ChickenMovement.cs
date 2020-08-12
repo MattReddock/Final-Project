88f2fa89f1f096011ffcc4757646a6e05c893807
MardukusHome <s191479@students.aie.edu.au>
2020-08-12 20:07:53 +0930

    added wandering to chicken and made it so it only wanders when its not running

--- Assets/Scripts/ChickenMovement.cs
+++ Assets/Scripts/ChickenMovement.cs
@@ -4,33 +4,42 @@ using UnityEngine;
 
 public class ChickenMovement : MonoBehaviour
 {
-	private Animator anim;
+    public float wanderRadius;
+    public float wanderTimer;
+    private float timer; 
+    private Transform target;
     private UnityEngine.AI.NavMeshAgent agent;
-    public GameObject player;
-    public float runAwayDistance = 8.0f;
-
-    private void Start()
+    
+ 
+    // Use this for initialization
+    void Start () 
     {
-        anim = GetComponent<Animator>();
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        timer = wanderTimer;
     }
-
-    private void Update()
+ 
+    // Update is called once per frame
+    void Update ()
     {
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-
-        if (distance < runAwayDistance)
-        {
-            Vector3 dirToPlayer = transform.position - player.transform.position;
-
-            Vector3 newPos = transform.position + dirToPlayer;
-
+        timer += Time.deltaTime;
+ 
+        if (timer >= wanderTimer) {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
             agent.SetDestination(newPos);
-            anim.SetBool("Run", true);
+            timer = 0;
         }
-        else
-        {
-            anim.SetBool("Run", false);
-        }
+    }
+ 
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+ 
+        randDirection += origin;
+ 
+        UnityEngine.AI.NavMeshHit navHit;
+ 
+        UnityEngine.AI.NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+ 
+        return navHit.position;
     }
 }
